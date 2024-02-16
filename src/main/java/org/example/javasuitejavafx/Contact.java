10ba3fb152b55dc37bb3757f25cf54e6171d6462
MGray2 <mgray24@basecampcodingacademy.org>
2024-02-16 14:25:49 -0600

    Updated ExpenseTracker

--- src/main/java/org/example/javasuitejavafx/Contact.java
+++ src/main/java/org/example/javasuitejavafx/Contact.java
@@ -1,5 +1,7 @@
 package org.example.javasuitejavafx;
 
+import java.util.ArrayList;
+
 public class Contact {
     String name;
     String email;
@@ -12,14 +14,32 @@ public class Contact {
         this.isFavorite = isFavorite;
     }
 
+
+
     public static void createContact(String name, String email, String phone, boolean isFavorite) {
         Contact newContact = new Contact(name, email, phone, isFavorite);
+
         // find a way to add this to PostgreSQL table
     }
 
-    public static Object[] contactList() {
+    public static ArrayList<String> contactList() {
         // returns an array of contacts
-        return null;
+        ArrayList list = new ArrayList<>();
+
+        // querys go here
+
+
+
+        return list;
     }
+
+
+
 }
+
+// delete
+
+// update
+
+
 
